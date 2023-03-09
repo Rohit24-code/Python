ce0216fbc873be218b1c148cfddf052d4ca3889d
Rohit singh <rohitsingh17031995@gmail.com>
2023-03-09 10:21:49 +0530

    If else using ternary and if./else

---
+++ meaning.py
@@ -0,0 +1,11 @@
+meaning = 23;
+print('')
+
+# if/else 
+if meaning>10:
+    print('Right on!')
+else:
+    print('Not today')
+
+# Ternary Operator 
+print('Right on with ternary operator') if meaning>10 else print("not today") 
