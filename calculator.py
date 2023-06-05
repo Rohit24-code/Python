7d0e912fa11be3220e0e6ba164825d37f78802f6
Rohit singh <rohitsingh17031995@gmail.com>
2023-06-05 11:58:51 +0530

    calculation

---
+++ calculator.py
@@ -0,0 +1,24 @@
+           # input with integers
+
+# x = int(input("What's x"))
+# y = int(input("What's Y"))
+ 
+# print(x+y)
+
+          # input with floats
+x = float(input("what's x ? "))
+y = float(input("what's y ? "))
+
+# round(number [,digit])
+# this [] means optional round to any no of digits
+
+# z = round(x+y)
+z=round(x/y,2)
+# print(z)
+# to make 1,000 while adding 999 to 1 we do
+# print(f"{z:,}")
+
+
+# This is used to  round digit to s digit
+# without using round function .
+# print(f"{z:.2f}")
