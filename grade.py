55d09bc5e3082b129b6ba669fd94d989f2f4699e
Rohit singh <rohitsingh17031995@gmail.com>
2023-06-06 10:40:21 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,12 @@
+score = int(input("give me your score ? "))
+
+if 90<=score <=100:
+    print("you got A grade")
+elif 80<=score <90:
+    print("you got B grade")
+elif 70<=score <80:
+    print("you got C grade")
+elif 60<=score <70:
+    print("you got D grade")
+else:
+    print("you got F grade")
