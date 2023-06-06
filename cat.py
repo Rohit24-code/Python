83ed73991ed3c5b0baa57b4184537061975aa104
Rohit singh <rohitsingh17031995@gmail.com>
2023-06-06 11:13:24 +0530

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,52 @@
+                            #   while loop 
+# i=3;
+# while i!=0:
+#     print("meow")
+#     i= i- 1
+
+                             # for loop
+# for _ in range(3):
+#     print("meow")
+
+                    #  can do this using print 
+# print("meow\n" *15 ,end="")
+
+# while True:
+#     n=int(input("Give me a number ? "))
+#     if n>0:
+#        break;
+
+
+# for _ in range(n):
+#     print("meow")
+
+
+
+                                     #FUnction that meows
+# def main():
+#     num=ask_no()
+#     meow(num)
+
+# def meow(n):
+#     for _ in range(n):
+#         print("meow")
+
+# def ask_no():
+#     while True:
+#        n =  int(input("Give me a number ? "))
+#        if n>0:
+#         return n
+
+
+# main()
+
+students = ["rohit","rahul","any"]
+
+for student in students:
+    print(student)
+
+
+# to print number with single student
+# len is for length
+for i in range(len(students)):
+    print(i+1,students[i])
