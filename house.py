3039af4c6c503bc96ad919944d5041e6456985a9
Rohit singh <rohitsingh17031995@gmail.com>
2023-06-06 10:39:37 +0530

    Add files via upload

---
+++ house.py
@@ -0,0 +1,7 @@
+name=input("Whats your name ? ")
+
+match name:
+    case "rohit" | "bro" | "me" : print("vo hum h")
+    case "yogesh sir" : print("QA")
+    case "shawon" : print("frontend")
+    case _:print("who?")
